ab188d5f139594866601d7d9f25a3fa81b8e0a2d
RoiGoujat <whysoserious_06@live.fr>
2018-09-11 18:47:47 +0200

    Big update on Email action handler

--- auth/action.js
+++ auth/action.js
@@ -44,10 +44,6 @@ document.addEventListener('DOMContentLoaded', function() {
       // Display reset password handler and UI.
       handleResetPassword(auth, actionCode, continueUrl, lang);
       break;
-    case 'recoverEmail':
-      // Display email recovery handler and UI.
-      handleRecoverEmail(auth, actionCode, lang);
-      break;
     case 'verifyEmail':
       // Display email verification handler and UI.
       handleVerifyEmail(auth, actionCode, continueUrl, lang);
@@ -57,34 +53,6 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 }, false);
 
-function handleRecoverEmail(auth, actionCode, lang) {
-  // Localize the UI to the selected language as determined by the lang
-  // parameter.
-  var restoredEmail = null;
-  // Confirm the action code is valid.
-  auth.checkActionCode(actionCode).then(function(info) {
-    // Get the restored email address.
-    restoredEmail = info['data']['email'];
-
-    // Revert to the old email.
-    return auth.applyActionCode(actionCode);
-  }).then(function() {
-    // Account email reverted to restoredEmail
-
-    // TODO: Display a confirmation message to the user.
-
-    // You might also want to give the user the option to reset their password
-    // in case the account was compromised:
-    auth.sendPasswordResetEmail(restoredEmail).then(function() {
-      // Password reset confirmation sent. Ask user to check their email.
-    }).catch(function(error) {
-      // Error encountered while sending password reset code.
-    });
-  }).catch(function(error) {
-    // Invalid code.
-  });
-}
-
 function handleResetPassword(auth, actionCode, continueUrl, lang) {
   // Localize the UI to the selected language as determined by the lang
   // parameter.
@@ -93,27 +61,32 @@ function handleResetPassword(auth, actionCode, continueUrl, lang) {
   auth.verifyPasswordResetCode(actionCode).then(function(email) {
     var accountEmail = email;
 
-    // TODO: Show the reset screen with the user's email and ask the user for
-    // the new password.
-
-    // Save the new password.
-    auth.confirmPasswordReset(actionCode, newPassword).then(function(resp) {
-      // Password reset has been confirmed and new password updated.
-
-      // TODO: Display a link back to the app, or sign-in the user directly
-      // if the page belongs to the same domain as the app:
-      // auth.signInWithEmailAndPassword(accountEmail, newPassword);
-
-      // TODO: If a continue URL is available, display a button which on
-      // click redirects the user back to the app via continueUrl with
-      // additional state determined from that URL's parameters.
-    }).catch(function(error) {
-      // Error occurred during confirmation. The code might have expired or the
-      // password is too weak.
-    });
+    document.getElementById('verifyAccount').style.display = 'block'
+  
   }).catch(function(error) {
     // Invalid or expired action code. Ask user to try to reset the password
     // again.
+  });
+}
+
+function resetPassword(actionCode) {
+
+  // Save the new password.
+  auth.confirmPasswordReset(actionCode, newPassword).then(function(resp) {
+
+
+    // Password reset has been confirmed and new password updated.
+
+    // TODO: Display a link back to the app, or sign-in the user directly
+    // if the page belongs to the same domain as the app:
+    // auth.signInWithEmailAndPassword(accountEmail, newPassword);
+
+    // TODO: If a continue URL is available, display a button which on
+    // click redirects the user back to the app via continueUrl with
+    // additional state determined from that URL's parameters.
+  }).catch(function(error) {
+    // Error occurred during confirmation. The code might have expired or the
+    // password is too weak.
   });
 }
 
